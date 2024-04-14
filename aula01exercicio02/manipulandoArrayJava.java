ecc9aedc5fa7f713aee31eb94c01e8d0300a6bcc
RODOLFO SOARES FERRAZ <115852778+rferraz1@users.noreply.github.com>
2024-04-14 20:06:52 -0300

    Update manipulandoArrayJava.java
    Atualização de tabela

--- aula01exercicio02/manipulandoArrayJava.java
+++ aula01exercicio02/manipulandoArrayJava.java
@@ -5,23 +5,21 @@ public class FileSystemPlayground {
 
     public static void main(String[] args) {
 
-        int[] valoresNumericos = { 10,20,30,40,50 };
-        String[] titulosMusicais = {"Smile - Kirk Franklin", 
-        "Superstition - Stivie Wonder", "Hotel California"};
-        
-       System.out.println("Valores Numericos: " + Arrays.toString(valoresNumericos));
-       System.out.println("Titulos de músicas:" + Arrays.toString(titulosMusicais));
-       
-       int somaValores = Arrays.stream(valoresNumericos).sum();
-       System.out.println("Soma dos valores numéricos: " + somaValores);
-       
-       String[] musicasComSuperstition = Arrays.stream(titulosMusicais) 
-                                        .filter(titulo -> titulo.contains("Superstition")
-                                        || titulo.contains("California")) 
-                                        .toArray(String[]::new);
-      System.out.println("Musicas com a palavbra 'Superstition' e 'California' no titulo: " + Arrays.toString(musicasComSuperstition));
+      int[][] myteste = {
+        {145,455,223},
+        {15,60,56},
+        {33,44,58},
+        {01,02,03}
+      };
+      int elemento = myteste[1][2];
+      System.out.println("O elemento é: " + elemento);
+      int maxLoop = myteste.length;
+      for (int i = 0; i < maxLoop ; i++)
+      {
+         System.out.println("linha: " + i);
+         System.out.println("O elemento no loop, na coluna 1 é: " + myteste[i][0]);
+         System.out.println("O elemento no loop, na coluna 2 é: " + myteste[i][1]);
+         System.out.println("O elemento no loop, na coluna 3 é: " + myteste[i][2]);
+       } 
     }
-  
 }
-
-
